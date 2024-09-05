070fed676bbcccd5e90aca2b3cdf7d88338bef3e
dennis casiguran <denniscasiguran@denniss-MacBook-Air.local>
2024-09-04 19:31:37 -0700

    first commit

---
+++ register.js
@@ -0,0 +1,22 @@
+console.log("Register");
+
+let pet=[];//empty array
+
+//creae the pet1 and pet2 (name, age, gender, breeed)
+
+let pet1={
+    name:"Chancho",
+    age:10,
+    gender:"male",
+    breed:"shitzu"
+}
+
+let pet2={
+    name:"Snow",
+    age:10,
+    gender:"male",
+    breed:"pomeranian"
+}
+
+document.write(pet1.name);
+document.write(pet2.name);
